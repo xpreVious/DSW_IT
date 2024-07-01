ca9d6e8ec7798c4eaf9374a21bc7c367ba64a806
Michał Słowiński <michal@czyrnek.net>
2024-07-01 17:45:42 +0200

    Add script.sh with basic functionalities

---
+++ script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+case "$1" in
+    --date|-d)
+        echo "$(date)"
+        ;;
+    --logs|-l)
+        if [ -z "$2" ]; then
+            count=100
+        else
+            count=$2
+        fi
+        for i in $(seq 1 $count); do
+            echo "Log number $i, created by skrypt.sh on $(date)" > "log_${i}.txt"
+        done
+        ;;
+    --help|-h)
+        echo "Usage: $0 [OPTION]"
+        echo "--date, -d       Show current date"
+        echo "--logs [NUM], -l [NUM]  Create NUM log files (default: 100)"
+        echo "--help, -h       Show this help message"
+        ;;
+    *)
+        echo "Invalid option. Use --help or -h for help."
+        ;;
+esac
